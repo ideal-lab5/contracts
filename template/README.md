07bba643be7de111f34dfeb8f0a47f95eb76f250
driemworks <tonyrriemer@gmail.com>
2023-12-06 15:40:39 -0600

    add template and examples

---
+++ template/README.md
@@ -0,0 +1,17 @@
+# Template
+
+This is a template to get you started with building contracts on the ETF network.
+
+To build and test this contract, use the cargo contract tool.
+
+## Building
+
+``` sh
+cargo +nightly contract build
+```
+
+## Testing
+
+``` sh
+cargo +nightly test
+```
---
+++ template/lib.rs
@@ -0,0 +1,55 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+use ink::prelude::vec::Vec;
+use etf_chain_extension::ext::EtfEnvironment;
+
+#[ink::contract(env = EtfEnvironment)]
+mod vickrey_auction {
+    use ink::storage::Mapping;
+    use scale::alloc::string::ToString;
+    use sha3::Digest;
+    use crate::EtfEnvironment;
+
+
+    #[derive(PartialEq, Debug, scale::Decode, scale::Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub enum Error {
+        /// the origin must match the configured proxy
+        AnError,
+    }
+
+    /// the auction storage
+    #[ink(storage)]
+    pub struct Template {
+        value: u8,
+    }
+
+    impl Template {
+    
+        /// Constructor that initializes a new auction
+        #[ink(constructor)]
+        pub fn new(value: u8) -> Self {
+            Self {
+                value
+            }
+        }
+
+        #[ink(message)]
+        pub fn get_value(&self) -> u8 {
+            self.value.clone()
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[ink::test]
+        fn it_works() {
+            let mut contract = Template::new(1u8);
+            assert_eq!(contract.get_value(), 1u8);
+        }
+    }
+}
